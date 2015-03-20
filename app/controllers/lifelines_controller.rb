8295a886cf3964b9681b75c16a095e3fce9a5ebc
Mallory Bucell <m.f.bucell@gmail.com>
2015-03-20 12:41:42 -0400

    better error handling, space for logging

--- app/controllers/lifelines_controller.rb
+++ app/controllers/lifelines_controller.rb
@@ -8,17 +8,17 @@ class LifelinesController < ApplicationController
     Lifeline.add_lifeline params[:user_id] 
     render json: { text: "BTW- sorry I haven\'t gotten back to you yet. I will do it soon.", username: "D.Zoolander" }
   rescue StandardError => e
-    #DO RAILSY THINGS
+    Rails.config.env.error_handler.(e)
     render json: { text: "Sorry, I can't do that thing you asked. I'm not sure what you're talking about.", username: "Mugatu" }
   end
 
   private
 
     def verify_slack_token!
-      ENV["WEBHOOK_SLACK"] == params[:token] #&& (User.lookup_by_slack_id params[:id])
+    ENV["WEBHOOK_SLACK"] == params[:token]
     rescue StandardError => e
-      #Do Railsy things
-      render json: { text: "BTW- Sorry, I can't find that thing earlier.", username: "Matilda" }
+      Rails.configuration.handle_errors.(e)
+      render json: { text: "BTW- Sorry, I can't find that thing earlier.", username: "Mugatu" }
     end
 
 end
