df320d47da96f4fec168ed258c1b3b7a5cc991a5
Mallory Bucell <m.f.bucell@gmail.com>
2015-03-20 10:53:49 -0400

    how stuff

--- app/controllers/lifelines_controller.rb
+++ app/controllers/lifelines_controller.rb
@@ -6,9 +6,10 @@ class LifelinesController < ApplicationController
 
   def create
 
-    if Lifeline.add_lifeline params[:user_id] 
+    Lifeline.add_lifeline params[:user_id] 
       render json: "BTW- sorry I haven't gotten back to you yet. I will do it soon."
-    else
+    rescue StandardError => e
+      #Rails
       render json: { text: "Sorry, I can't do that thing you asked. I'm not sure what you're talking about." }
     end
   end
@@ -16,9 +17,11 @@ class LifelinesController < ApplicationController
   private
 
     def verify_slack_token!
-       render json: "BTW- Sorry, I couldn't find that thing earlier." unless (ENV["SLASH_SLACK"] == params[:token]) #&& (User.lookup_by_slack_id params[:id])
-      # redirect_to errors_path
-      # raise User::InvalidRequestError, "I could not find what you were looking for." 
+      (ENV["SLASH_SLACK"] == params[:token])
+      rescue StadardError => e
+        #Rails
+        render json: "BTW- Sorry, I couldn't find that thing you were looking for."
+      end
     end
 
 end
