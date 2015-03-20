9e2e2d3a72a440ebb57d1ccf21ef6d6cb8c70648
Mallory Bucell <m.f.bucell@gmail.com>
2015-03-19 22:08:53 -0400

    updated env

--- app/controllers/lifelines_controller.rb
+++ app/controllers/lifelines_controller.rb
@@ -9,14 +9,14 @@ class LifelinesController < ApplicationController
     if Lifeline.add_lifeline params[:user_id] 
       render json: "BTW- sorry I haven't gotten back to you yet. I will do it soon."
     else
-      render json: "Sorry, I can't do that thing you asked. I'm not sure what you're talking about."
+      render json: { text: "Sorry, I can't do that thing you asked. I'm not sure what you're talking about." }
     end
   end
 
   private
 
     def verify_slack_token!
-       render json: "BTW- Sorry, I couldn't find that thing earlier." unless (ENV["TRUE_SLACK"] == params[:token]) && (User.lookup_by_slack_id params[:id])
+       render json: "BTW- Sorry, I couldn't find that thing earlier." unless (ENV["SLASH_SLACK"] == params[:token]) #&& (User.lookup_by_slack_id params[:id])
       # redirect_to errors_path
       # raise User::InvalidRequestError, "I could not find what you were looking for." 
     end
