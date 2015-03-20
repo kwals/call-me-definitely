46def01ea166e0b9f7823c1448b4fc1e11507bf3
Mallory Bucell <m.f.bucell@gmail.com>
2015-03-19 22:42:54 -0400

    working webhook responses

--- app/controllers/lifelines_controller.rb
+++ app/controllers/lifelines_controller.rb
@@ -5,18 +5,17 @@ class LifelinesController < ApplicationController
   before_action :verify_slack_token!, only: [:create]
 
   def create
-
     if Lifeline.add_lifeline params[:user_id] 
-      render json: "BTW- sorry I haven't gotten back to you yet. I will do it soon."
+      render json: { text: "BTW- sorry I haven't gotten back to you yet. I will do it soon.", username: "D.Zoolander" }
     else
-      render json: "Sorry, I can't do that thing you asked. I'm not sure what you're talking about."
+      render json: { text: "Sorry, I can't do that thing you asked. I'm not sure what you're talking about.", username: "Mugatu" }
     end
   end
 
   private
 
     def verify_slack_token!
-       render json: "BTW- Sorry, I couldn't find that thing earlier." unless (ENV["TRUE_SLACK"] == params[:token]) && (User.lookup_by_slack_id params[:id])
+      render json: { text: "BTW- Sorry, I can't find that thing earlier.", username: "Melanie" } unless (ENV["WEBHOOK_SLACK"] == params[:token]) && (User.lookup_by_slack_id params[:id])
       # redirect_to errors_path
       # raise User::InvalidRequestError, "I could not find what you were looking for." 
     end
