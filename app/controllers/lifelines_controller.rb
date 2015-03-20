51414b9511b6087c9500c9e7a86001f1f47ddcd4
Mallory Bucell <m.f.bucell@gmail.com>
2015-03-20 11:12:36 -0400

    working rescue setup

--- app/controllers/lifelines_controller.rb
+++ app/controllers/lifelines_controller.rb
@@ -5,19 +5,20 @@ class LifelinesController < ApplicationController
   before_action :verify_slack_token!, only: [:create]
 
   def create
-    if Lifeline.add_lifeline params[:user_id] 
-      render json: { text: "BTW- sorry I haven't gotten back to you yet. I will do it soon.", username: "D.Zoolander" }
-    else
-      render json: { text: "Sorry, I can't do that thing you asked. I'm not sure what you're talking about.", username: "Mugatu" }
-    end
+    Lifeline.add_lifeline params[:user_id] 
+    render json: { text: "BTW- sorry I haven\'t gotten back to you yet. I will do it soon.", username: "D.Zoolander" }
+  rescue StandardError => e
+    #DO RAILSY THINGS
+    render json: { text: "Sorry, I can't do that thing you asked. I'm not sure what you're talking about.", username: "Mugatu" }
   end
 
   private
 
     def verify_slack_token!
-      render json: { text: "BTW- Sorry, I can't find that thing earlier.", username: "Matilda" } unless (ENV["WEBHOOK_SLACK"] == params[:token]) #&& (User.lookup_by_slack_id params[:id])
-      # redirect_to errors_path
-      # raise User::InvalidRequestError, "I could not find what you were looking for." 
+      ENV["WEBHOOK_SLACK"] == params[:token] #&& (User.lookup_by_slack_id params[:id])
+    rescue StandardError => e
+      #Do Railsy things
+      render json: { text: "BTW- Sorry, I can't find that thing earlier.", username: "Matilda" }
     end
 
 end
